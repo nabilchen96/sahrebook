38105aa6a4e4c3a5c7d6da366053320647819843
nabilchen96 <escendol900@gmail.com>
2022-08-24 08:52:22 +0700

    update

--- resources/views/backend/components/navbar.blade.php
+++ resources/views/backend/components/navbar.blade.php
@@ -5,6 +5,7 @@
                 <i class="icon-grid menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
             </a>
+            
         </li>
         <li class="nav-item">
             <a class="nav-link" href="{{ url('back/user') }}">
