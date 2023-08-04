41a98195c52393f098b481a51b7c07667c6efd94
nabilchen96 <escendol900@gmail.com>
2023-08-04 20:07:08 +0700

    update

--- resources/views/backend/detail-produk/edit.blade.php
+++ resources/views/backend/detail-produk/edit.blade.php
@@ -58,7 +58,7 @@
                         </div>
                         <div class="mb-3">
                             <label class="form-label">Isi Pembahasan <sup class="text-danger">*</sup></label>
-                            <div id="editor">
+                            <div id="editor" style="height: 500px;">
                                 <?php
                                     $charactersToRemove = ['&Acirc;', '&acirc;', '&#157;', '&#140;', '&#156;', '&#143;', '&iuml;', '&cedil;', '&#143;'];    
                                 ?>
